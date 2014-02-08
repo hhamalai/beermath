a61e325967433afc17e1e69e4d3e108f4e8e614d
Harri Hämäläinen <hhamalai@iki.fi>
2014-02-08 15:31:05 +0200

    fixed sane line lengths

--- beermath.erl
+++ beermath.erl
@@ -22,15 +22,19 @@ lbs2kgs(Lbs) -> Lbs * 0.4536.
 %       - Proportion: Proportion of the malt from the total amount of malts
 % VolumeLitras: Totals volume in litres
 % TargetGravity: The target gravity
-malt_bill(Malts, VolumeLitres, TargetGravity) -> malt_bill2(Malts, litres2gallons(VolumeLitres) * TargetGravity).
-malt_bill2(Malts, TotalGravity) -> [{Malt, lbs2kgs(Proportion * TotalGravity / Efficiency / ExtractPotential) } || {Malt, ExtractPotential, Efficiency, Proportion} <- Malts].
+malt_bill(Malts, VolumeLitres, TargetGravity) 
+    -> malt_bill2(Malts, litres2gallons(VolumeLitres) * TargetGravity).
+malt_bill2(Malts, TotalGravity) 
+    -> [{Malt, lbs2kgs(Proportion * TotalGravity / Efficiency / ExtractPotential) }
+        || {Malt, ExtractPotential, Efficiency, Proportion} <- Malts].
 
 
 %% Gravity calculations
 % Gravity Temperature Correction by Lyons (1992)
 gravity_correction(Celcius) -> gravity_correction(fahrenheit, c2f(Celcius)).
 
-gravity_correction(fahrenheit, Fahrenheit) -> 1.313454 - 0.132674 * Fahrenheit + 0.002057793 * math:pow(Fahrenheit, 2) - 0.000002627634 * math:pow(Fahrenheit, 3).
+gravity_correction(fahrenheit, Fahrenheit)
+    -> 1.313454 - 0.132674 * Fahrenheit + 0.002057793 * math:pow(Fahrenheit, 2) - 0.000002627634 * math:pow(Fahrenheit, 3).
 
 %% IBU calculations
 % Boil time factor
@@ -47,5 +51,6 @@ aa_util(Mins, WortGravity) -> bigness_factor(WortGravity) * boil_time_factor(Min
 
 aa_concentration(AlphaAcidRating, Grams, Volume) -> AlphaAcidRating * Grams * 1000 / Volume.
 
-ibus(AlphaAcidRating, Grams, Volume, Mins, WortGravity) -> aa_util(Mins, WortGravity) * aa_concentration(AlphaAcidRating, Grams, Volume).
+ibus(AlphaAcidRating, Grams, Volume, Mins, WortGravity)
+    -> aa_util(Mins, WortGravity) * aa_concentration(AlphaAcidRating, Grams, Volume).
 
